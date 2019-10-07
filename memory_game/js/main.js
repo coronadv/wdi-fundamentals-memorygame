a7da09b5da4bb56e2957ae4870e63c575a6a1fb4
Ray Plaza <munditoplaza@gmail.com>
2019-10-06 23:37:58 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,26 +2,20 @@ const cards = ["queen", "queen", "king", "king"];
 
 const cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push('cardOne');
-
-console.log("user flipped queen");
-
-
-
-
-var cardTwo = cards[2];
-
-cardsInPlay.push('cardTwo');
-
-console.log("user flipped king");
-
-
-
-
-if (cardsInPlay.length === 2) {
-  alert("You found a match!");
+function checkForMatch() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-  alert("Sorry, try again");
+  console.log("Sorry, try again.");
 }
+}
+
+function flipCard(cardId){
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+
+
+flipCard(0)
+flipCard(2)
